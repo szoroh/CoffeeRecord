9be130e8ac0374964b6eff6f7590f91e7fc8d98a
szoroh <kubahorosz95@gmail.com>
2019-04-13 20:59:32 +0200

    Add coffee new and create

---
+++ app/controllers/coffees_controller.rb
@@ -0,0 +1,30 @@
+class CoffeesController < ApplicationController
+  def index
+    @coffees = Coffee.all
+  end
+
+  def new
+    @coffee = Coffee.new
+  end
+
+  def create
+    @coffee = Coffee.new(coffee_params)
+
+    if @coffee.save
+      flash[:success] = 'Coffee successfully created'
+      redirect_to coffees_path
+    else
+      flash[:error] = @coffee.errors.full_messages.to_sentence
+      redirect_to new_coffee_path
+    end
+  end
+
+  private
+
+  def coffee_params
+    params.require(:coffee).permit(:name, :roasters, :country, :region,
+                                   :cultivation_altitude, :variety,
+                                   :treatment_method, :package_quantity,
+                                   :brew_method, :flavour_profile, :roast_date)
+  end
+end
--- spec/system/coffee_system_spec.rb
+++ spec/system/coffee_system_spec.rb
@@ -11,4 +11,53 @@ RSpec.describe "Coffee resource", type: :system do
       expect(page).to have_text("Friedhats")
     end
   end
+
+  describe "create" do
+    context "with all data filled in properly" do
+      it "creates coffee profile correctly" do
+        visit "/coffees/new"
+
+        fill_in "Name",                 with: "Ehtiopia Jajmejo"
+        fill_in "Roasters",             with: "Friedhats"
+        fill_in "Country",              with: "Ethiopia"
+        fill_in "Region",               with: "Oromia"
+        fill_in "Cultivation altitude", with: 1800
+        fill_in "Variety",              with: "Heirloom"
+        fill_in "Treatment method",     with: "Washed"
+        fill_in "Package quantity",     with: 250
+        fill_in "Brew method",          with: "Aeropress"
+        fill_in "Flavour profile",      with: "Citrus"
+        fill_in "Roast date",           with: 01/01/2019
+
+         click_button "Submit"
+
+        expect(page.current_path).to eq(coffees_path)
+        expect(page).to have_content("Coffee successfully created")
+      end
+    end
+
+    context "with missing name" do
+      it "fails to create a record" do
+        visit "/coffees/new"
+
+
+        fill_in "Name",                 with: ""
+        fill_in "Roasters",             with: "Friedhats"
+        fill_in "Country",              with: "Ethiopia"
+        fill_in "Region",               with: "Oromia"
+        fill_in "Cultivation altitude", with: 1800
+        fill_in "Variety",              with: "Heirloom"
+        fill_in "Treatment method",     with: "Washed"
+        fill_in "Package quantity",     with: 250
+        fill_in "Brew method",          with: "Aeropress"
+        fill_in "Flavour profile",      with: "Citrus"
+        fill_in "Roast date",           with: 01/01/2019
+
+        click_button "Submit"
+
+        expect(page.current_path).to eq(new_coffee_path)
+        expect(page).to have_content("Name can't be blank")
+      end
+    end
+  end
 end
