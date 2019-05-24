917ab4903b464815659a41513b811119d028ca70
szoroh <kubahorosz95@gmail.com>
2019-05-24 23:19:54 +0200

    Add model associations

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,4 +3,6 @@
 class Coffee < ApplicationRecord
   validates :name, presence: true
   validates :roasters, presence: true
+
+  has_many :recipes
 end
---
+++ db/migrate/20190517074430_add_coffee_to_recipes.rb
@@ -0,0 +1,5 @@
+class AddCoffeeToRecipes < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :recipes, :coffee, index: true, foreign_key: true
+  end
+end
