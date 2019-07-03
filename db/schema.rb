8023faea870b9e80029280779ce55ea4945baa08
szoroh <kubahorosz95@gmail.com>
2019-07-03 13:08:52 +0200

    Add associations for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_074430) do
+ActiveRecord::Schema.define(version: 2019_07_03_103406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_074430) do
     t.integer "cultivation_altitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_coffees_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -35,7 +37,9 @@ ActiveRecord::Schema.define(version: 2019_05_17_074430) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "coffee_id"
+    t.bigint "user_id"
     t.index ["coffee_id"], name: "index_recipes_on_coffee_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,5 +60,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_074430) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "coffees", "users"
   add_foreign_key "recipes", "coffees"
+  add_foreign_key "recipes", "users"
 end
