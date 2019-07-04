efdcb5c000fabc17a85e7616fc094b89e9ddfe2f
szoroh <kubahorosz95@gmail.com>
2019-07-04 13:16:23 +0200

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
   validates :name, presence: true
+
+  has_many :coffees, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 end
